953f5b14634c228e93eb0a05f38254682789eb45
hesham <hesham.mohamed9131@gmail.com>
2026-09-16 20:40:39 +0300

    Initial Commit

---
+++ EcommerceTraining/EcommerceTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceTrainingApp.swift
+//  EcommerceTraining
+//
+//  Created by hesham on 16/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcommerceTrainingTests/EcommerceTrainingTests.swift
@@ -0,0 +1,19 @@
+//
+//  EcommerceTrainingTests.swift
+//  EcommerceTrainingTests
+//
+//  Created by hesham on 16/09/2026.
+//
+
+import Testing
+@testable import EcommerceTraining
+
+struct EcommerceTrainingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
